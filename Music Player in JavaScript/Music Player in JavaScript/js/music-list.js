50320780743e1cd52936e32c7126917efe529678
TheRyfox <126391184+TheRyfox@users.noreply.github.com>
2023-12-24 18:12:46 +0100

    Add files via upload

---
+++ Music Player in JavaScript/Music Player in JavaScript/js/music-list.js
@@ -0,0 +1,44 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Only Fans Remix",
+    artist: "Lunay, Myke Towers, Jhay Cortez, Arcangel, Darell, Brray",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ponte en Posicion",
+    artist: "Darell y Ñengo Flow!",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Ñengo Flow - Whyne Whini",
+    artist: "Ñengo Flow - Whyne Whini",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Pacto (Remix)",
+    artist: "Jay Wheeler, Anuel AA, Hades66",
+    img: "music-4",
+    src: "music-4"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
